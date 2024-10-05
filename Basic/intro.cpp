0e5642d60793b25c4da23b2667dcfc095ae0f3c1
raulsync <rahul.aanand086@gmail.com>
2024-10-05 16:25:27 +0530

    basic_data_type_taking_input_output

---
+++ Basic/intro.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//print output
+
+int main(){
+
+   int a = 10, b = 12;
+
+  cout << a << " "<<b <<"\n"  ;
+//data type int long long long double float char bool
+
+
+  // int a = 1;
+  char c = 'b';
+  double d = 123453;
+  float e = 1.244245;
+  long long int f = 12345567678997;
+
+  cout << c <<" "<< d<< " "<< e <<" "<<f <<"\n";
+
+
+
+//arithmetic operator :- + - * / %
+
+  cout << 7/2 << " " << 7%2+1 <<"\n ";
+
+
+  //taking input
+
+  int age;
+  cout <<"enter your age"<<"\n";
+  cin>> age;
+  cout <<"Your age is "<<age;
+
+  char x;
+  cout <<"enter character" <<": ";
+  cin>> x;
+
+  cout<<" "<<x;
+
+return 0;
+}
