c8edf21fc4629ae7977c9837396b70217168788f
raulsync <rahul.aanand086@gmail.com>
2025-02-28 22:31:34 +0530

    2darr:intro

---
+++ 2Darray/intro.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // inittalize array as row
+  // int arr[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+  // initializing array as where we want to specify which element goes to which row
+
+  int arr[3][4] = {{1, 11, 111, 1111}, {2, 22, 222, 2222}, {3, 33, 333, 3333}};
+
+  // taking input
+
+  // for (int row = 0; row < 3; row++)
+  // {
+  //   for (int col = 0; col < 4; col++)
+  //   {
+  //     cin >> arr[row][col];
+  //   }
+  // }
+
+  // output as row
+
+  for (int row = 0; row < 3; row++)
+  {
+    for (int col = 0; col < 4; col++)
+    {
+      cout << arr[row][col] << " ";
+    }
+    cout << endl;
+  }
+
+  // // output as col;
+  // for (int col = 0; col < 4; col++)
+  // {
+  //   for (int row = 0; row < 3; row++)
+  //   {
+  //     cout << arr[row][col] << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  return 0;
+}
